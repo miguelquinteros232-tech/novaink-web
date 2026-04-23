b9aba14cd6ce49935416a9b6c6cbc98649c91602
miguelquinteros232-tech <miguelquinteros232@gmail.com>
2026-04-22 23:33:19 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Nova Ink',
+  description: 'Sublimación y Estampados',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  )
+}
