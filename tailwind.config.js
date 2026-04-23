0f9ee04042ae7105abdaf567bd42852c2fe422fa
miguelquinteros232-tech <miguelquinteros232@gmail.com>
2026-04-23 00:03:33 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,17 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        // Aquí definimos los colores exactos de tu imagen
-        'nova-black': '#0D0D0D',   // Fondo oscuro profundo
-        'nova-card': '#161616',    // Fondo de los cuadritos
-        'nova-green': '#00FFA3',   // El verde neón brillante
-        'nova-border': '#2A2A2A',  // El color de los bordes sutiles
+      animation: {
+        'pulse-slow': 'pulse 6s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       },
-      borderRadius: {
-        'nova': '2.5rem', // Los bordes bien redondeados que te gustaron
-      }
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
